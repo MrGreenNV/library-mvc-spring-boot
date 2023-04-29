6654c4936de9175fb440707f5a8a97f5fa8726e1
”MrGreenNV” <averkievnv@gmail.com>
2023-04-29 19:08:57 +0300

    init

---
+++ src/main/java/ru/averkiev/libraryboot/LibrarybootApplication.java
@@ -0,0 +1,13 @@
+package ru.averkiev.libraryboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibrarybootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibrarybootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/averkiev/libraryboot/LibrarybootApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.averkiev.libraryboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibrarybootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
